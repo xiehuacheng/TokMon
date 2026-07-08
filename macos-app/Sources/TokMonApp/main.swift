2b75a18f70f73113a7d9b296f56d1b33dc38e597
xiehuacheng <xiehuacheng@agree.com.cn>
2026-07-08 13:45:24 +0800

    Add Edit menu to enable standard paste shortcuts

--- macos-app/Sources/TokMonApp/main.swift
+++ macos-app/Sources/TokMonApp/main.swift
@@ -28,6 +28,23 @@ final class TokMonApplicationDelegate: NSObject, NSApplicationDelegate {
     removeAppearanceObserver()
   }
 
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    configureMainMenu()
+  }
+
+  private func configureMainMenu() {
+    let mainMenu = NSMenu()
+    let editMenuItem = NSMenuItem(title: "Edit", action: nil, keyEquivalent: "")
+    let editMenu = NSMenu(title: "Edit")
+    editMenu.addItem(withTitle: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
+    editMenu.addItem(withTitle: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
+    editMenu.addItem(withTitle: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
+    editMenu.addItem(withTitle: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
+    editMenuItem.submenu = editMenu
+    mainMenu.addItem(editMenuItem)
+    NSApplication.shared.mainMenu = mainMenu
+  }
+
   func applicationDidResignActive(_ notification: Notification) {
     guard statusPanel?.isVisible == true else {
       return
